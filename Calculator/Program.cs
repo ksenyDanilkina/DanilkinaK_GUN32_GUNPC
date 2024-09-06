dc948f2f2776d367f4caa2b3c28fe258bc0763f1
Danilkina Kseniya <hostel1992@mail.ru>
2024-09-06 17:09:13 +0700

    init commit. Task added

---
+++ Calculator/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine("Enter first number: ");
+
+if (!int.TryParse(Console.ReadLine(), out var number1))
+{
+    Console.WriteLine("Error! Enter an integer");
+    return;
+}
+
+Console.WriteLine("Enter second number: ");
+
+if (!int.TryParse(Console.ReadLine(), out var number2))
+{
+    Console.WriteLine("Error! Enter an integer");
+    return;
+}
+
+Console.WriteLine("Enter operator(& | or ^): ");
+var operand = Console.ReadLine();
+
+if (!(operand== "&" || operand == "|" || operand == "^"))
+{
+    Console.WriteLine("Error! Enter correct operator");
+    return;
+}
+
+switch (operand)
+{
+    case "&":
+        Console.WriteLine($"Result {number1}{operand}{number2} in binary: {Convert.ToString(number1&number2,2)}");
+        Console.WriteLine($"Result {number1}{operand}{number2} in decimal: {Convert.ToString(number1 & number2, 10)}");
+        Console.WriteLine($"Result {number1}{operand}{number2} in hexadecimal: {Convert.ToString(number1 & number2, 16)}");
+        break;
+    case "|":
+        Console.WriteLine($"Result {number1}{operand}{number2} in binary: {Convert.ToString(number1 | number2, 2)}");
+        Console.WriteLine($"Result {number1}{operand}{number2} in decimal: {Convert.ToString(number1 | number2, 10)}");
+        Console.WriteLine($"Result {number1}{operand}{number2} in hexadecimal: {Convert.ToString(number1 | number2, 16)}");
+        break;
+    case "^":
+        Console.WriteLine($"Result {number1}{operand}{number2} in binary: {Convert.ToString(number1 ^ number2, 2)}");
+        Console.WriteLine($"Result {number1}{operand}{number2} in decimal: {Convert.ToString(number1 ^ number2, 10)}");
+        Console.WriteLine($"Result {number1}{operand}{number2} in hexadecimal: {Convert.ToString(number1 ^ number2, 16)}");
+        break;
+}
